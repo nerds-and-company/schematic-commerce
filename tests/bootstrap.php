fb78f58a27e2a0a15cb6ffc609d79287ed9b1308
Bob Olde Hampsink <b.oldehampsink@nerds.company>
2017-04-19 09:05:54 +0200

    Require needed files

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -6,5 +6,10 @@ $rootPath = getenv('TRAVIS') ? __DIR__.'/../' : __DIR__.'/../../../../';
 require_once $rootPath.'craft/app/tests/bootstrap.php';
 require_once CRAFT_APP_PATH.'Info.php';
 
+// Require Craft commerce
+require_once $rootPath.'craft/plugins/commerce/models/Commerce_ProductTypeModel.php';
+require_once $rootPath.'craft/plugins/commerce/models/Commerce_ProductTypeLocaleModel.php';
+require_once $rootPath.'craft/plugins/commerce/services/Commerce_ProductTypesService.php';
+
 // Require autoloader
 require_once $rootPath.'vendor/autoload.php';
