d1642ba10e6fd0dec5dcbb3823101ef0451b457b
Bob Olde Hampsink <b.oldehampsink@nerds.company>
2017-05-01 14:56:49 +0200

    Improve shipping methods coverage

--- tests/Services/ShippingMethodsTest.php
+++ tests/Services/ShippingMethodsTest.php
@@ -3,7 +3,10 @@
 namespace NerdsAndCompany\Schematic\Commerce\Services;
 
 use Craft\BaseTest;
+use Craft\Commerce_ShippingCategoryModel;
 use Craft\Commerce_ShippingMethodModel;
+use Craft\Commerce_ShippingRuleModel;
+use Craft\Commerce_ShippingRuleCategoryModel;
 use Craft\Commerce_ShippingMethodsService;
 use Craft\Craft;
 use Craft\DbCommand;
@@ -155,7 +158,7 @@ class ShippingMethodsTest extends BaseTest
     /**
      * @param string $methodId
      *
-     * @return Mock|ShippingMethodModel
+     * @return Mock|Commerce_ShippingMethodModel
      */
     private function getMockShippingMethod($methodId)
     {
@@ -173,7 +176,7 @@ class ShippingMethodsTest extends BaseTest
 
         $mockShippingMethod->expects($this->any())
             ->method('getRules')
-            ->willReturn([]);
+            ->willReturn([$this->getMockShippingRule($methodId)]);
 
         $mockShippingMethod->expects($this->any())
             ->method('getAllErrors')
@@ -185,7 +188,77 @@ class ShippingMethodsTest extends BaseTest
     }
 
     /**
-     * @return Mock|CategoriesService
+     * @param string $ruleId
+     *
+     * @return Mock|Commerce_ShippingRuleModel
+     */
+    private function getMockShippingRule($ruleId)
+    {
+        $mockShippingRule = $this->getMockBuilder(Commerce_ShippingRuleModel::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mockShippingRule->expects($this->any())
+            ->method('__get')
+            ->willReturnMap([
+                ['id', $ruleId],
+                ['name', 'ruleName'.$ruleId],
+            ]);
+
+        $mockShippingRule->expects($this->any())
+            ->method('getShippingRuleCategories')
+            ->willReturn([$this->getMockShippingRuleCategory($ruleId)]);
+
+        return $mockShippingRule;
+    }
+
+    /**
+     * @param string $categoryId
+     *
+     * @return Mock|Commerce_ShippingRuleCategoryModel
+     */
+    private function getMockShippingRuleCategory($categoryId)
+    {
+        $mockShippingRuleCategory = $this->getMockBuilder(Commerce_ShippingRuleCategoryModel::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mockShippingRuleCategory->expects($this->any())
+            ->method('__get')
+            ->willReturnMap([
+                ['id', $categoryId],
+            ]);
+
+        $mockShippingRuleCategory->expects($this->any())
+            ->method('getCategory')
+            ->willReturn([$this->getMockShippingCategory($categoryId)]);
+
+        return $mockShippingRuleCategory;
+    }
+
+    /**
+     * @param string $categoryId
+     *
+     * @return Mock|Commerce_ShippingCategoryModel
+     */
+    private function getMockShippingCategory($categoryId)
+    {
+        $mockShippingCategory = $this->getMockBuilder(Commerce_ShippingCategoryModel::class)
+            ->disableOriginalConstructor()
+            ->getMock();
+
+        $mockShippingCategory->expects($this->any())
+            ->method('__get')
+            ->willReturnMap([
+                ['id', $categoryId],
+                ['handle', 'categoryHandle'.$categoryId],
+            ]);
+
+        return $mockShippingCategory;
+    }
+
+    /**
+     * @return Mock|Commerce_ShippingMethodsService
      */
     private function setMockShippingMethodsService()
     {
